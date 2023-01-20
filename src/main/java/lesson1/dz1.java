36773a135ad83fff904b524c7ffa897bbdfdebb0
Sergei904025 <105377873+Sergei904025@users.noreply.github.com>
2023-01-20 19:27:52 +0600

    Initial commit

---
+++ src/main/java/lesson1/dz1.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+public class dz1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        
+        Testvars();
+        
+    }
+
+    private static void Testvars() {
+    }
+
+
+}
