e99cacb7cd074431bd5497dc06b60fa96c3a4c0f
Sergei904025 <105377873+Sergei904025@users.noreply.github.com>
2023-01-25 12:08:28 +0600

    commit1

--- src/main/java/lesson1/dz1.java
+++ src/main/java/lesson1/dz1.java
@@ -5,11 +5,19 @@ public class dz1 {
         System.out.println("hello");
         
         Testvars();
-        
+
     }
 
+
     private static void Testvars() {
+        int a = 3;
+        int b = 4;
+        int sum = (a +b);
+        System.out.println("никогда не сдавайся!");
+
     }
+
+
 
 
 }
